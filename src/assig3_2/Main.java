b891f65bd64c8d33af0f2755bd54b64f6990eabc
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-01 20:44:48 +0200

    Update Main.java

--- src/assig3_2/Main.java
+++ src/assig3_2/Main.java
@@ -1,15 +1,12 @@
 package assig3_2;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         GamePlay game = new GamePlay(); // creates new game
         Gamer player1 = new Gamer(game);
-        Gamer player2 = new Gamer(game);
+        Gamer player2 = new Gamer(game); // creates two player.
         player1.start();
-       
         player2.start();
-
         game.getJudge().start();
         player1.join();
         player2.join();
@@ -20,6 +17,5 @@ public class Main {
             System.out.println("player 2 wins");
         else
             System.out.println("tie");
-
     }
 }
