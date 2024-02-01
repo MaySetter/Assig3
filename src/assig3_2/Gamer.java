5a0c9ee4fa20e4a9186dcc11e9d6a35f91141b32
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-01 18:28:22 +0200

    Update Gamer.java

--- src/assig3_2/Gamer.java
+++ src/assig3_2/Gamer.java
@@ -1,54 +1,55 @@
 package assig3_2;
 
-/**
- * The Gamer class represents a player thread who flips the coin repeatedly
- */
-public class Gamer extends Thread{
+public class Gamer extends Thread {
 
-    private int goodFlipsCounter; // Counts the number successful flips that the player made
+    private int goodFlipsCounter; // Counts the number of successful flips that the player made
     private GamePlay gamePlay;
 
     /**
-     * Constructor initializes gamePLay variable.
+     * Constructor initializes gamePlay variable.
+     *
      * @param gp GamePlay object.
      */
-    public Gamer(GamePlay gp){
+    public Gamer(GamePlay gp) {
         gamePlay = gp;
+        this.goodFlipsCounter=0;
     }
 
     /**
-     * Method runs a while loop. In every iteration, as long as no INTERRUPT
-     * and the number of coin flips in the whole game is less or equal to 10:
-     * Tries flipping a coin, and if succeeds, advances goodFlipsCounter by -1,
+     * Method runs a while loop. In every iteration, as long as no INTERRUPT and the number of coin flips in the whole game is less or equal to 10:
+     * Tries flipping a coin, and if succeeds, advances goodFlipsCounter by 1,
      * and go to sleep for one second.
      */
-    public void play(){
-         while (!Thread.interrupted() && gamePlay.getNumOfRounds() <= 10) {
+    @SuppressWarnings("static-access")
+	public void play() {
+        while (!Thread.interrupted() && gamePlay.getNumOfRounds() <= 10) {
             // Try flipping a coin
-            int flipResult = gamePlay.flipCoin();
-
             // If the flip succeeds, increment the goodFlipsCounter
-            this.goodFlipsCounter=this.goodFlipsCounter+flipResult;
+            this.goodFlipsCounter=this.goodFlipsCounter+gamePlay.flipCoin();;//flipcoin() return 1 if succeed and 0 if not succeed.
 
             try {
                 // Sleep for one second
-                Thread.sleep(1000);
+               Thread.currentThread().sleep(1000);
             } catch (InterruptedException e) {
                 // Handle interrupted exception if needed
-                e.printStackTrace();
+               System.out.println(e.getMessage());
             }
         }
     }
 
     /**
      * Getter
+     *
      * @return number of successful coin flips.
      */
-    public int getScore(){
+    public int getScore() {
         return goodFlipsCounter;
     }
-
-    public void run(){
+/**
+ * Run method to start play .
+ * must implements to use Gamer as Thread.
+ */
+    public void run() {
         this.play();
     }
 }
