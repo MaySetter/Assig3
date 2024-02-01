c36d04c136e63bde30225b87d6f36408f3370dc4
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-01 18:26:17 +0200

    Update Main.java
    Add start Judge

--- src/assig3_2/Main.java
+++ src/assig3_2/Main.java
@@ -6,14 +6,14 @@ public class Main {
         GamePlay game = new GamePlay(); // creates new game
         Gamer player1 = new Gamer(game);
         Gamer player2 = new Gamer(game);
-        Judge judge=new Judge(game);
         player1.start();
+       
         player2.start();
-        judge.start();
 
-       player1.join();
-       player2.join();
-       judge.join();
+        game.getJudge().start();
+        player1.join();
+        player2.join();
+        game.getJudge().join();
         if (player1.getScore() > player2.getScore())
             System.out.println("player 1 wins");
         else if (player1.getScore() < player2.getScore())
