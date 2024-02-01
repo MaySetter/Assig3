cc469e0d4d22124be3568093fa2bcb052b777599
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-01 18:27:36 +0200

    Update Judge.java
    checking if coin is available or not and change

--- src/assig3_2/Judge.java
+++ src/assig3_2/Judge.java
@@ -5,33 +5,43 @@ package assig3_2;
  * flip the coin, by making the coin available/unavailable
  */
 public class Judge extends Thread {
-	private GamePlay gp;
+	private GamePlay gamePlay;
     public Judge(GamePlay game) {
-		this.gp=game;
+		this.gamePlay=game;
 	}
 
 	/**
      * A loop, in which as long as the thread was not interrupted, the Judge makes the coin
      * unavailable for a second and then makes the coin available for half a second.
      */
-    public void judge() {
+    @SuppressWarnings("static-access")
+	public void judge() {
+    	
         try {
-            while (!Thread.interrupted()) {
-                // Make the coin unavailable for a second
-                this.gp.makeCoinAvail(false);
-                Judge.sleep(1000);
-
-                // Make the coin available for half a second
-                this.gp.makeCoinAvail(true);
-                Judge.sleep(500);
+            while (!Thread.interrupted() && gamePlay.getNumOfRounds() <= 10) {
+            
+               // Make the coin unavailable for a second
+                if(this.gamePlay.getCoinStatus()) {
+                	synchronized(this.gamePlay) {
+                	this.gamePlay.makeCoinAvail(false);
+                	Thread.currentThread().sleep(1000);
+                	}
+                }
+              // Make the coin available for half a second
+                else {
+                	synchronized(this.gamePlay) {
+                		this.gamePlay.makeCoinAvail(true);
+                		Thread.currentThread().sleep(500);
+                	}
+            	}
             }
         } catch (InterruptedException e) {
             // Handle interrupted exception if needed
-            e.printStackTrace();
+        	System.out.println(e.getMessage());
         }
     }
 
-    public void run() {
-        this.judge();
+	public void run() {
+       this.judge();
     }
 }
