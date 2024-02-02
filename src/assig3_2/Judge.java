3b0b75fdb62c4a7d51ed678b29178096f06b750b
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-02 12:00:43 +0200

    Update Judge.java
    Add notes ... final

--- src/assig3_2/Judge.java
+++ src/assig3_2/Judge.java
@@ -1,37 +1,50 @@
-//@author Nir Hazan 316009489 , May Seter 312123037
+// Nir Hazan 316009489 , May Seter 312123037
 package assig3_2;
-
 /**
  * The Judge class represents a judge in the game, who lets the Gamers know when they can
  * flip the coin, by making the coin available/unavailable
+ * @author Nir Hazan 316009489 , May Seter 312123037
  */
 public class Judge extends Thread {
 	private GamePlay gamePlay;
-
-	public Judge(GamePlay game) {
-		this.gamePlay = game;
+    public Judge(GamePlay game) {
+		this.gamePlay=game;
 	}
 
 	/**
      * A loop, in which as long as the thread was not interrupted, the Judge makes the coin
      * unavailable for a second and then makes the coin available for half a second.
      */
-
+	@SuppressWarnings("static-access")
+	public void judge() {
+    	 while (!Thread.interrupted() && gamePlay.getNumOfRounds() <= 10) {
+    		 try {
+               // Make the coin unavailable for a second than judge sleep
+                if(!this.gamePlay.getCoinStatus()) {
+                	
+                	this.gamePlay.makeCoinAvail(true);
+                	System.out.println("Coin available for half second");
+					Thread.currentThread().sleep(500);;
+                
+                	}
+              // Make the coin available for half a second then judge sleep
+                else {
+                		this.gamePlay.makeCoinAvail(false);
+                		System.out.println("Coin unavailable for one second");
+                		Thread.currentThread().sleep(1000);
+                		
+                	}
+            	}catch (InterruptedException e) {
+            // Handle interrupted exception if needed
+        	System.out.println(e.getMessage());
+        }
+    	 }
+    }
+	/**
+	 * run method  that start judge 
+	 * must implement to use Judge as a thread.
+	 */
 	public void run() {
-		try {
-			while (!Thread.interrupted() && gamePlay.getNumOfRounds() < 10) {
-				synchronized(this.gamePlay) {
-					this.gamePlay.makeCoinAvail(true);
-					Judge.currentThread().sleep(500);
-					System.out.println("Coin available for half second");
-					this.gamePlay.makeCoinAvail(false);
-					System.out.println("Coin unavailable for one second");
-					Judge.currentThread().sleep(1000);
-				}
-			}
-		} catch (InterruptedException e) {
-			// Handle interrupted exception if needed
-			System.out.println(e.getMessage());
-		}
+       this.judge();
     }
 }
