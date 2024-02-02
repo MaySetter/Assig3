a1847729ea733365e5e1b40f3718d42830c9a633
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-02 12:02:02 +0200

    Update Main.java
    Add notes ... final

--- src/assig3_2/Main.java
+++ src/assig3_2/Main.java
@@ -1,23 +1,26 @@
-//@author Nir Hazan 316009489 , May Seter 312123037
+//Nir Hazan 316009489 , May Seter 312123037
 package assig3_2;
+/**
+ * Main class to run the game.
+ * @author Nir Hazan 316009489 , May Seter 312123037
+ *
+ */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        GamePlay game = new GamePlay(); // creates new game.
-        Gamer player1 = new Gamer(game);
-        Gamer player2 = new Gamer(game); // creates two players.
 
-        // start
+        GamePlay game = new GamePlay(); // creates new game
+        Gamer player1 = new Gamer(game);
+        Gamer player2 = new Gamer(game); // creates two player.
+        //start
         player1.start();
         player2.start();
-        game.getJudge().start();
-
-        // join
+        game.getJudge().start();  // start Threads .
+        //join
         player1.join();
         player2.join();
         game.getJudge().join();
-
-        // print winner
-        if (player1.getScore() > player2.getScore())
+        //print winner or tie.
+        if (player1.getScore() > player2.getScore()) // end of the game check who wins or tie.
             System.out.println("player 1 wins");
         else if (player1.getScore() < player2.getScore())
             System.out.println("player 2 wins");
