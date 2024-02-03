57f3ce51bc7c9532438159ea82e9dac92499e606
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-03 18:11:59 +0200

    Update SlicerThread.java
    with functions

--- src/assig3_3/SlicerThread.java
+++ src/assig3_3/SlicerThread.java
@@ -5,5 +5,24 @@ package assig3_3;
  * SlicerThread class is responsible for slicing the vegetables.
  */
 public class SlicerThread extends Thread {
-
+	private SlicerMachine slicerMachine;
+	public SlicerThread(SlicerMachine sm) {
+		this.slicerMachine=sm;
+	}
+	public void run() {
+		synchronized(this.slicerMachine) {
+			while(this.slicerMachine.getNumOfPreparedSalads()<this.slicerMachine.getNumOfSaladsToPrepare()) {
+		while(this.slicerMachine.getNumOfCucumbers()!=this.slicerMachine.cucumbersNeededForOneSalad||this.slicerMachine.getNumOfTomatoes()!=this.slicerMachine.tomatoesNeededForOneSalad) {
+			try {
+				System.out.println("Cant make salad yet  cells are not full.");
+				this.slicerMachine.wait();
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
+		this.slicerMachine.sliceVegetables();
+			
+		}
+		}	
+	}
 }
