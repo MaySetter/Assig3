08b9586fe01981d988d5eee0ae550fc568adc831
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-03 18:11:37 +0200

    Update SlicerMachine.java
    with functions

--- src/assig3_3/SlicerMachine.java
+++ src/assig3_3/SlicerMachine.java
@@ -1,52 +1,88 @@
 //@author Nir Hazan 316009489 , May Seter 312123037
 package assig3_3;
 
+import java.util.Scanner;
+
 /**
  * SlicerMachine class represents the functions of the machine.
  */
 public class SlicerMachine {
-	
-	int numOfCucumbers = 0;
-	int numOfTomatoes = 0;
-	int numOfPreparedSalads = 0;
-	
+	Scanner scan=new Scanner(System.in);
+	int numOfCucumbers ;
+	int numOfTomatoes;
+	int numOfPreparedSalads ;
+	int numOfSaladToPrepare;
 	final int cucumbersNeededForOneSalad = 3;
 	final int tomatoesNeededForOneSalad = 2;
-	
+	public SlicerMachine(int num) {
+	 numOfCucumbers = 0;
+	 numOfTomatoes = 0;
+	 numOfPreparedSalads = 0;
+	 this.numOfSaladToPrepare=num;
+		
+	}
 	// add one cucumber into the slicer chamber
-	void addOneCucumber() {
+	public synchronized void addOneCucumber() {
 		if (numOfCucumbers < cucumbersNeededForOneSalad) {
 			System.out.println("adding one cucumber to the machine");
 			numOfCucumbers++;
+			if(numOfCucumbers==this.cucumbersNeededForOneSalad)
+				notifyAll();
 		}
 	}
 
 	// add one tomato into the slicer chamber
-	void addOneTomato() {
+	public synchronized void addOneTomato() {
 		if (numOfTomatoes < tomatoesNeededForOneSalad) {
 			System.out.println("adding one tomato to the machine");
 			numOfTomatoes++;
+			if(numOfTomatoes==this.tomatoesNeededForOneSalad)
+				notifyAll();
 		}
 	}
 	
 	// if there are enough vegetables in the slicer
 	// chamber, make another salad
-	void sliceVegetables() {
+	public synchronized void sliceVegetables() {
 		if ((numOfCucumbers >= cucumbersNeededForOneSalad) && (numOfTomatoes >= tomatoesNeededForOneSalad)) {
 			makeNewSalad();
 		}
 	}
 
-	private void makeNewSalad() {
+	private synchronized  void makeNewSalad() {
 		System.out.println("== preparing one more salad ==");
 		numOfPreparedSalads++; 
 		// update stock
 		numOfTomatoes = numOfTomatoes - tomatoesNeededForOneSalad;
 		numOfCucumbers = numOfCucumbers - cucumbersNeededForOneSalad;
+		notifyAll();
 	}	
 	
 	int getNumOfPreparedSalads() {
 		return numOfPreparedSalads;
 	}
+/**
+ * Getter.
+ * @return int - Number of cucumbers in machine
+ */
+	public int getNumOfCucumbers() {
+		return this.numOfCucumbers;
+	}
+	/**
+	 * Getter.
+	 * @return int - Number of tomatoes in machine
+	 */
+		public int getNumOfTomatoes() {
+			return this.numOfTomatoes;
+		}
+		/**
+		 * Getter.
+		 * @return int - number of salads need to be prepare 
+		 */
+	public int getNumOfSaladsToPrepare() {
+		
+		return this.numOfSaladToPrepare;
+	}
 
+}
 }
