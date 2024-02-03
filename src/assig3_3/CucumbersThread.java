95c84867bfe2a829a7d8e48bcaf2a538b5898a07
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-03 18:10:38 +0200

    Update CucumbersThread.java
    with functions

--- src/assig3_3/CucumbersThread.java
+++ src/assig3_3/CucumbersThread.java
@@ -5,5 +5,23 @@ package assig3_3;
  * CucumbersThread class is responsible for feeding cucumbers to the slicing machine.
  */
 public class CucumbersThread extends Thread {
-
+	private SlicerMachine slicerMachine;
+	public CucumbersThread(SlicerMachine sm) {
+		this.slicerMachine=sm;
+	}
+	public void run() {
+		synchronized(this.slicerMachine){
+		while(this.slicerMachine.getNumOfPreparedSalads()<this.slicerMachine.getNumOfSaladsToPrepare()) {
+			while(this.slicerMachine.getNumOfCucumbers()==this.slicerMachine.cucumbersNeededForOneSalad) {
+			try {
+				System.out.println("Cucumbers cell is full.");
+				this.slicerMachine.wait();
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}	
+		}
+		this.slicerMachine.addOneCucumber();
+	}
+	}
+}
 }
