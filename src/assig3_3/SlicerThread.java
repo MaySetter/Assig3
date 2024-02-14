461495ce742fe8262f1244bceef52129d664733a
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-14 10:47:29 +0200

    Update SlicerThread.java
    notes

--- src/assig3_3/SlicerThread.java
+++ src/assig3_3/SlicerThread.java
@@ -5,7 +5,7 @@ package assig3_3;
  * SlicerThread class is responsible for slicing the vegetables.
  */
 public class SlicerThread extends Thread {
-	private final SlicerMachine slicerMachine;
+	private final SlicerMachine slicerMachine; // Slicer machine only represend the cells no need to be changed-final.
 
 	public SlicerThread(SlicerMachine sm) {
 		this.slicerMachine = sm;
@@ -14,20 +14,20 @@ public class SlicerThread extends Thread {
 	public void run() {
 		synchronized (this.slicerMachine) {
 			while (this.slicerMachine.getNumOfPreparedSalads() < this.slicerMachine.getNumOfSaladsToPrepare()) {
-				while (veggiesNotReady()) {
+				while (veggiesNotReady()) {  // if not 3 cucumbers and 2 tomato need to wait.
 					try {
 						this.slicerMachine.wait();
 					} catch (InterruptedException e) {
 						System.out.println(e.getMessage());
 					}
 				}
-				this.slicerMachine.sliceVegetables();
+				this.slicerMachine.sliceVegetables(); // if cells are full cut 1 salad.
 			}
 		}
-		currentThread().interrupt();
+		currentThread().interrupt(); // after finsih all the salads ask thread to stop (cancelletion).
 	}
 
-	private boolean veggiesNotReady(){
+	private boolean veggiesNotReady(){ // check if there is 3 cucumbers and 2 tomatos in the machine.
 		return (this.slicerMachine.getNumOfCucumbers() != this.slicerMachine.getCucumbersNeededForOneSalad()
 				|| this.slicerMachine.getNumOfTomatoes() != this.slicerMachine.getTomatoesNeededForOneSalad());
 	}
