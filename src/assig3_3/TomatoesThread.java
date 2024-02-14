c7695aba5aa2c6239bad939dd0f7553346786d02
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-14 10:43:45 +0200

    Update TomatoesThread.java
    notes

--- src/assig3_3/TomatoesThread.java
+++ src/assig3_3/TomatoesThread.java
@@ -5,7 +5,7 @@ package assig3_3;
  * TomatoesThread class is responsible for feeding tomatoes to the slicing machine.
  */
 public class TomatoesThread extends Thread {
-	private final SlicerMachine slicerMachine;
+	private final SlicerMachine slicerMachine; // Slicer machine only cuts no need to be changed-final.
 	public TomatoesThread(SlicerMachine sm) {
 
 		this.slicerMachine = sm;
@@ -14,11 +14,11 @@ public class TomatoesThread extends Thread {
 	public void run() {
 		while (!isInterrupted()){
 			synchronized (this.slicerMachine) {
-				try {
+				try {     // if tomato cell is full go to wait.
 					while (this.slicerMachine.getNumOfTomatoes() == slicerMachine.getTomatoesNeededForOneSalad()) {
 						this.slicerMachine.wait();
 					}
-					this.slicerMachine.addOneTomato();
+					this.slicerMachine.addOneTomato(); // if there is a place add one tomato to the cell.
 				} catch (InterruptedException e) {}
 			}
 		}
