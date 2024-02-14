2acf3b75d15afa3ad086c72e3c1f7ca22dcaedce
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-02-14 10:42:03 +0200

    Update CucumbersThread.java
    notes

--- src/assig3_3/CucumbersThread.java
+++ src/assig3_3/CucumbersThread.java
@@ -5,20 +5,20 @@ package assig3_3;
  * CucumbersThread class is responsible for feeding cucumbers to the slicing machine.
  */
 public class CucumbersThread extends Thread {
-	private final SlicerMachine slicerMachine;
+	private final SlicerMachine slicerMachine;   // Slicer machine only cuts no need to be changed-final.
 	public CucumbersThread(SlicerMachine sm) {
 
 		this.slicerMachine = sm;
 	}
 
 	public void run(){
-		while (!isInterrupted()) {
+		while (!isInterrupted()) {    
 			synchronized (this.slicerMachine) {
-				try {
+				try {               // if cucumber cell is full go to wait.
 					while (this.slicerMachine.getNumOfCucumbers() == slicerMachine.getCucumbersNeededForOneSalad()) {
 						this.slicerMachine.wait();
 					}
-					this.slicerMachine.addOneCucumber();
+					this.slicerMachine.addOneCucumber(); // if there is a place add one cucumber to the cell.
 				} catch (InterruptedException e) {}
 			}
 		}
